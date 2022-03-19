f1a6beaa47ff21e5d5bbfe29af5729d1bba034a6
otrapersona <otrapersonagit@riseup.net>
2022-03-19 17:08:52 -0600

    Formatted keyboardarduinomidi.ino. Respectable readme.

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # keyboardarduinomidi
-usb keyboard -> arduino -> midi 
+
+Through Arduino, use a USB keyboard as a MIDI controller for a music device.
+
+Tested on Genius SlimStar 130 (GK-150001) keyboard, Arduino UNO R3 board, Volca Kick analogue kick generator (firmware v 1.05, date 2019-09-10).
+
+Note: I learned *some* music theory in middle school, sorry if something for any music crime.
+
+## Setup
+
+### Libraries
+
+You'll need the `MIDI` library by Francois Best.\
+It's downloaded through the Arduino IDE library manager.
+
+### Keyboard-Arduino connection
+
+[Source](https://create.arduino.cc/projecthub/michalin70/connect-a-usb-keyboard-with-an-arduino-50c077 "Connect a USB Keyboard with an Arduino - Arduino Project Hub") and [source](https://create.arduino.cc/projecthub/Ahmedebeed555/connect-arduino-uno-to-usb-keyboard-2f01f8 "Connect Arduino UNO to USB Keyboard - Arduino Project Hub")
+
+| Keyboard | arduino | Resistor |
+|----------|---------|----------|
+| 5v       | 5v      | no       |
+| Gnd      | Gnd     | no       |
+| Data+    | 7       | 10k Ω    |
+| Data-    | 6       | 10k Ω    |
+
+### MIDI-Arduino connection
+
+[Source](https://docs.arduino.cc/built-in-examples/communication/Midi "MIDI Note Player | Arduino Documentation | Arduino Documentation")
+
+| MIDI | Arduino | Resistor	|
+|------|---------|----------|
+| 5    | 1       | 220 Ω    |
+| 2    | Gnd     | no       |
+| 4    | 5V      | 220 Ω    |
+
+## Functions
+
+### Music keyboard
+
+[Source](https://newt.phys.unsw.edu.au/jw/notes.html "Note names, MIDI numbers and frequencies")
+
+The next section (Octave selector) addresses changing octave/pitch.
+
+| Key | Note       | Note alt name     |
+|-----|------------|-------------------|
+| z   | do         | C                 |
+| s   | re♭ / do♯  | D minor / C mayor |
+| x   | re         | D                 |
+| d   | mi♭ / re♯  | E minor / D mayor |
+| C   | mi         | E                 |
+| V   | fa         | F                 |
+| G   | sol♭ / fa♯ | G minor / C mayor |
+| B   | sol        | G                 |
+| H   | la♭ / sol♯ | A minor / C mayor |
+| N   | la         | A                 |
+| J   | si♭ / la♯  | B minor / C mayor |
+| M   | si         | B                 |
+
+### Octave selector
+
+Default = 1.\
+Press one of this keys to change the octave of the keyboard (pitch?).\
+Note: You might want to switch to octave 4 first, it's the middle one, [source](https://en.wikipedia.org/wiki/Octave "Octave - Wikipedia").
+
+| key (numpad) | octave |
+|--------------|--------|
+| 0            | 0      |
+| 1            | 1      |
+| 2            | 2      |
+| 3            | 3      |
+| 4            | 4      |
+| 5            | 5      |
+| 6            | 6      |
+| 7            | 7      |
+| 8            | 8      |
--- keyboardarduinomidi.ino
+++ keyboardarduinomidi.ino
@@ -16,29 +16,149 @@ void setup()
 	pinMode(DATA, INPUT);
 }
 
-void doh(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(12+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(12+12*mult, 0, 1);} }
+void doh(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(12 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(12 + 12 * mult, 0, 1);
+	}
+}
 
-void reb(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(13+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(13+12*mult, 0, 1);} }
+void reb(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(13 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(13 + 12 * mult, 0, 1);
+	}
+}
 
-void re(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(14+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(14+12*mult, 0, 1);} }
+void re(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(14 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(14 + 12 * mult, 0, 1);
+	}
+}
 
-void mib(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(15+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(15+12*mult, 0, 1);} }
+void mib(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(15 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(15 + 12 * mult, 0, 1);
+	}
+}
 
-void mi(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(16+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(16+12*mult, 0, 1);} }
+void mi(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(16 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(16 + 12 * mult, 0, 1);
+	}
+}
 
-void fa(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(17+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(17+12*mult, 0, 1);} }
+void fa(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(17 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(17 + 12 * mult, 0, 1);
+	}
+}
 
-void solb(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(18+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(18+12*mult, 0, 1);} }
+void solb(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(18 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(18 + 12 * mult, 0, 1);
+	}
+}
 
-void sol(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(19+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(19+12*mult, 0, 1);} }
+void sol(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(19 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(19 + 12 * mult, 0, 1);
+	}
+}
 
-void lab(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(20+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(20+12*mult, 0, 1);} }
+void lab(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(20 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(20 + 12 * mult, 0, 1);
+	}
+}
 
-void la(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(21+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(21+12*mult, 0, 1);} }
+void la(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(21 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(21 + 12 * mult, 0, 1);
+	}
+}
 
-void sib(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(22+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(22+12*mult, 0, 1);} }
+void sib(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(22 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(22 + 12 * mult, 0, 1);
+	}
+}
 
-void si(bool onoff, int mult) { if (onoff == true) {MIDI.sendNoteOn(23+12*mult, 127, 1);} else if (onoff == false) {MIDI.sendNoteOff(23+12*mult, 0, 1);} }
+void si(bool onoff, int mult)
+{
+	if (onoff == true)
+	{
+		MIDI.sendNoteOn(23 + 12 * mult, 127, 1);
+	}
+	else if (onoff == false)
+	{
+		MIDI.sendNoteOff(23 + 12 * mult, 0, 1);
+	}
+}
 
 void loop()
 {
@@ -60,36 +180,36 @@ void loop()
 		switch (curr)
 		{
 		case 0x70:
-      octave = 0;
-      break;
-    case 0x69:
-      octave = 1;
-      break;
-    case 0x72:
-      octave = 2;
-      break;
-    case 0x7A:
-      octave = 3;
-      break;
-    case 0x6B:
-      octave = 4;
-      break;
-    case 0x73:
-      octave = 5;
-      break;
-    case 0x74:
-      octave = 6;
-      break;
-    case 0x6C:
-      octave = 7;
-      break;
-    case 0x75:
-      octave = 8;
-      break;
-    case 0x1A:
-      doh(true, octave);
-      break;
-    case 0x1B:
+			octave = 0;
+			break;
+		case 0x69:
+			octave = 1;
+			break;
+		case 0x72:
+			octave = 2;
+			break;
+		case 0x7A:
+			octave = 3;
+			break;
+		case 0x6B:
+			octave = 4;
+			break;
+		case 0x73:
+			octave = 5;
+			break;
+		case 0x74:
+			octave = 6;
+			break;
+		case 0x6C:
+			octave = 7;
+			break;
+		case 0x75:
+			octave = 8;
+			break;
+		case 0x1A:
+			doh(true, octave);
+			break;
+		case 0x1B:
 			reb(true, octave);
 			break;
 		case 0x22:
@@ -125,47 +245,49 @@ void loop()
 		}
 		lastplayed = curr;
 	}
- if (prev == 0xF0)
- {switch (curr)
-    {
-    case 0x1A:
-      doh(false, octave);
-      break;
-    case 0x1B:
-      reb(false, octave);
-      break;
-    case 0x22:
-      re(false, octave);
-      break;
-    case 0x23:
-      mib(false, octave);
-      break;
-    case 0x21:
-      mi(false, octave);
-      break;
-    case 0x2A:
-      fa(false, octave);
-      break;
-    case 0x34:
-      solb(false, octave);
-      break;
-    case 0x32:
-      sol(false, octave);
-      break;
-    case 0x33:
-      lab(false, octave);
-      break;
-    case 0x31:
-      la(false, octave);
-      break;
-    case 0x3B:
-      sib(false, octave);
-      break;
-    case 0x3A:
-      si(false, octave);
-      break;
-    }
-      lastplayed = 0x0;
-      prev = 0x0;}
+	if (prev == 0xF0)
+	{
+		switch (curr)
+		{
+		case 0x1A:
+			doh(false, octave);
+			break;
+		case 0x1B:
+			reb(false, octave);
+			break;
+		case 0x22:
+			re(false, octave);
+			break;
+		case 0x23:
+			mib(false, octave);
+			break;
+		case 0x21:
+			mi(false, octave);
+			break;
+		case 0x2A:
+			fa(false, octave);
+			break;
+		case 0x34:
+			solb(false, octave);
+			break;
+		case 0x32:
+			sol(false, octave);
+			break;
+		case 0x33:
+			lab(false, octave);
+			break;
+		case 0x31:
+			la(false, octave);
+			break;
+		case 0x3B:
+			sib(false, octave);
+			break;
+		case 0x3A:
+			si(false, octave);
+			break;
+		}
+		lastplayed = 0x0;
+		prev = 0x0;
+	}
 	prev = curr;
 }
